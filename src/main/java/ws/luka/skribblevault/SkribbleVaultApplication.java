64171bae3f407f58eb5a5a56bfe4f09bee058e54
Luka Žagar <luka.zagar@eng.it>
2023-12-22 13:24:37 +0100

    Blank project

---
+++ src/main/java/ws/luka/skribblevault/SkribbleVaultApplication.java
@@ -0,0 +1,13 @@
+package ws.luka.skribblevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkribbleVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkribbleVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ws/luka/skribblevault/SkribbleVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.luka.skribblevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkribbleVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
